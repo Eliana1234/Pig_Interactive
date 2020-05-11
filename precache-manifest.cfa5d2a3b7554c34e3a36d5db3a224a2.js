6e1338115da8da1e24910b4774385a2e5ec94f2b
Eliana Miller <elianasmiller@gmail.com>
2020-05-11 13:40:30 -0400

    Updates

---
+++ precache-manifest.cfa5d2a3b7554c34e3a36d5db3a224a2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a652412b6c529c72bd9db9c14d4dd26",
+    "url": "/Pig_Interactive/index.html"
+  },
+  {
+    "revision": "530a9b1bec7a7c250d95",
+    "url": "/Pig_Interactive/static/css/main.86461781.chunk.css"
+  },
+  {
+    "revision": "b1fc935efd007fdff8bf",
+    "url": "/Pig_Interactive/static/js/2.73895582.chunk.js"
+  },
+  {
+    "revision": "9e48934018617995d1c863d2e80915ec",
+    "url": "/Pig_Interactive/static/js/2.73895582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530a9b1bec7a7c250d95",
+    "url": "/Pig_Interactive/static/js/main.964cf975.chunk.js"
+  },
+  {
+    "revision": "45c8bcc4462386c20f8d",
+    "url": "/Pig_Interactive/static/js/runtime-main.f5e051d7.js"
+  },
+  {
+    "revision": "7c87ab24fb13f13bf3dff3ae17396f73",
+    "url": "/Pig_Interactive/static/media/check.7c87ab24.png"
+  },
+  {
+    "revision": "dddae67bed99261ca1c11c9fe965c24b",
+    "url": "/Pig_Interactive/static/media/chimpanz.dddae67b.png"
+  },
+  {
+    "revision": "29dd30f49b44091d6323bdf8e22a3324",
+    "url": "/Pig_Interactive/static/media/chimpcheck.29dd30f4.png"
+  },
+  {
+    "revision": "e1734616f9e4a8b47ff1d3ce8c9a59f1",
+    "url": "/Pig_Interactive/static/media/chimpx.e1734616.png"
+  },
+  {
+    "revision": "4d021e0bfa5f9d39b60e78fb2be719b4",
+    "url": "/Pig_Interactive/static/media/dogcheck.4d021e0b.png"
+  },
+  {
+    "revision": "990205b22deec1496192d9c517f3a4e7",
+    "url": "/Pig_Interactive/static/media/doggy.990205b2.png"
+  },
+  {
+    "revision": "91aa6f789ceb1169a501bef706273e2e",
+    "url": "/Pig_Interactive/static/media/dogx.91aa6f78.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pig_Interactive/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1b624e3f7915cf91c6dfa2219da247a5",
+    "url": "/Pig_Interactive/static/media/pig.1b624e3f.png"
+  },
+  {
+    "revision": "5ecacfa13a5c2edba7f521dc74bd06b5",
+    "url": "/Pig_Interactive/static/media/pigbackground.5ecacfa1.jpeg"
+  },
+  {
+    "revision": "521daf1d6a028bc213e51d3d30ef536c",
+    "url": "/Pig_Interactive/static/media/pigcheck.521daf1d.png"
+  },
+  {
+    "revision": "62adb4188236d817769a77fd2312902d",
+    "url": "/Pig_Interactive/static/media/pigx.62adb418.png"
+  },
+  {
+    "revision": "d7d635391d751af92f9be71706ce2761",
+    "url": "/Pig_Interactive/static/media/x-mark.d7d63539.png"
+  }
+]);
